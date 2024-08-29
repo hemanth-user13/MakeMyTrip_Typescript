071be2198721cbd1639de80bf4847b2bf96b6059
John Doe <john.doe@example.com>
2024-08-29 18:37:09 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Main from './Components/Main/MainPage'
+
+function App() {
+
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Main/MainPage.tsx
@@ -0,0 +1,11 @@
+import Navbar1 from "../Header/Navbar"
+
+function MainPage() {
+  return (
+    <div>
+      <Navbar1/>
+    </div>
+  )
+}
+
+export default MainPage
